6b408f8dc649c9b644e947fff1504eb396b7f712
orcaamsaveni <amsaveni.iiit@gmail.com>
2014-08-27 15:27:24 +0530

    pg

---
+++ pgm32.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main (void)
+{
+   printf ("pgm is fun. \n");
+   printf ("and pgm in c is even more fun.\n");
+
+   return 0;
+}
+   
---
+++ pro31.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main (void)
+{
+    printf (" programming is function. \n");
+    
+    return 0;
+}
